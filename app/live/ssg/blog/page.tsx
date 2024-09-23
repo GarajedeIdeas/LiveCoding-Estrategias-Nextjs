802fb4c41211583c3f2b8f86301ce80374fc672f
Gonzalo_Cuadros <gonzalo.cuadros.sge@mango.com>
2024-09-24 00:27:56 +0200

    feat(live-coding): cleaning live examples

--- app/live/ssg/blog/page.tsx
+++ app/live/ssg/blog/page.tsx
@@ -10,14 +10,5 @@ interface Post {
 const BLOG_URL = 'https://api.vercel.app/blog'
 
 export default async function Page() {
-  const data = await fetch(BLOG_URL)
-  const posts: Post[] = await data.json()
-
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  )
+  return <ul></ul>
 }
